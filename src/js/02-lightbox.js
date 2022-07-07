ac9b7f4a4edf7275810dcc906e769c561ac855ff
Kartavchenko <stranger1301078@gmail.com>
2022-07-07 12:47:13 +0200

    js-hw-07-02

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -9,9 +9,9 @@ galleryEl.insertAdjacentHTML("beforeend", markupGallery);
 
 function createImgmarkup(galleryItems) {
   return galleryItems.map(({ preview, original, description }) => {
-    return `<a class="gallery__item" href="${original}">
+    return `<li><a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>`;
+</a></li>`;
   }).join("");
 }
 console.log(createImgmarkup(galleryItems));
